b922826ffaeedce4ec1f67a4b5009b723f08acc0
Ítalo H <italodssouza.backup@gmail.com>
2023-03-20 15:41:12 -0300

    Update IPokemonRepository.cs

--- Interfaces/IPokemonRepository.cs
+++ Interfaces/IPokemonRepository.cs
@@ -5,5 +5,8 @@ namespace PokemonReview.Interfaces
     public interface IPokemonRepository
     {
         ICollection<Pokemon> GetPokemons();
+        Pokemon GetPokemon(int id);
+        Pokemon GetPokemon(string name);
+        bool PokemonExists(int id);
     }
 }
