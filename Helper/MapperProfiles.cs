5c93d47f17531d869311676e828861034ed2f1f0
Ítalo H <italodssouza.backup@gmail.com>
2023-03-20 15:40:08 -0300

    Adding AutoMappers

---
+++ Helper/MapperProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PokemonReview.Dto;
+using PokemonReview.Models;
+
+namespace PokemonReview.Helper
+{
+    public class MapperProfiles : Profile
+    {
+        public MapperProfiles()
+        {
+            CreateMap<Pokemon, PokemonDto>();
+        }
+    }
+}
