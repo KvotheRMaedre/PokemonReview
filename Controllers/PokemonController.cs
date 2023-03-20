b12ea1df155c71beb68dafa7d7c3bb832ca62030
Ítalo H <italodssouza.backup@gmail.com>
2023-03-20 15:41:06 -0300

    Adding Routes to Pokemon
    Searchin pokemon by id

--- Controllers/PokemonController.cs
+++ Controllers/PokemonController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReview.Dto;
 using PokemonReview.Interfaces;
 using PokemonReview.Models;
 
@@ -9,16 +11,35 @@ namespace PokemonReview.Controllers
     public class PokemonController : Controller
     {
         private readonly IPokemonRepository _pokemonRepository;
-        public PokemonController(IPokemonRepository pokemonRepository)
+        private readonly IMapper _mapper;
+
+        public PokemonController(IPokemonRepository pokemonRepository, IMapper mapper)
         {
             _pokemonRepository = pokemonRepository;
+            _mapper = mapper;
         }
 
         [HttpGet]
         [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
         public IActionResult getPokemons()
         {
-            var pokemons = _pokemonRepository.GetPokemons();
+            var pokemons = _mapper.Map<List<PokemonDto>>(_pokemonRepository.GetPokemons());
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(pokemons);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+        [ProducesResponseType(400)]
+        public IActionResult getPokemon(int id)
+        {
+            if(!_pokemonRepository.PokemonExists(id))
+                return NotFound();
+
+            var pokemons = _mapper.Map<PokemonDto>(_pokemonRepository.GetPokemon(id));
 
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
--- Repository/PokemonRepository.cs
+++ Repository/PokemonRepository.cs
@@ -1,6 +1,7 @@
 ﻿using PokemonReview.Data;
 using PokemonReview.Interfaces;
 using PokemonReview.Models;
+using System.Runtime.Intrinsics.Arm;
 
 namespace PokemonReview.Repository
 {
@@ -13,9 +14,24 @@ namespace PokemonReview.Repository
             _context = context;
         }
 
+        public Pokemon GetPokemon(int id)
+        {
+            return _context.Pokemons.Where(pokemon => id == pokemon.Id).FirstOrDefault();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            return _context.Pokemons.Where(pokemon => name == pokemon.Name).FirstOrDefault();
+        }
+
         public ICollection<Pokemon> GetPokemons()
         {
-            return _context.Pokemons.OrderBy(p => p.Id).ToList();
+            return _context.Pokemons.OrderBy(pokemon => pokemon.Id).ToList();
+        }
+
+        public bool PokemonExists(int id)
+        {
+            return _context.Pokemons.Any(pokemon => id == pokemon.Id);
         }
     }
 }
